f8d59d914dfcbf144cec56161a9f51cfe0d4c18d
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-27 07:52:25 +0800

    Add basic new command file

---
+++ src/NewCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bright\Installer\Console;
+
+use ZipArchive;
+use RuntimeException;
+use GuzzleHttp\Client;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+class NewCommand extends Command
+{
+
+}
