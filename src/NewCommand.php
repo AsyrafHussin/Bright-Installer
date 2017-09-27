f9594bb8a22994b63b5bd757ade98891b3c82620
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-27 08:12:18 +0800

    Add excute method with basic output display

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -28,4 +28,24 @@ class NewCommand extends Command
             ->setDescription('Create a new Bright application.')
             ->addArgument('name', InputArgument::OPTIONAL);
     }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // check php zip extension
+         if (! class_exists('ZipArchive')) {
+             throw new RuntimeException('The Zip PHP extension is not installed. Please install it and try again.');
+         }
+         
+         // get directory name
+         $directory = ($input->getArgument('name')) ? getcwd().'/'.$input->getArgument('name') : getcwd();
+
+         $output->writeln('<info>Creating Bright project...</info>');
+    }
 }
